c9357969d9ef3f8bde3bd310294c49c8b05e70ad
Pranya Agrawal <pranya.ag@gmail.com>
2026-07-11 00:36:31 +0530

    Add simple interest calculation

---
+++ Basics/calculate_simple_interest.py
@@ -0,0 +1,7 @@
+p = float(input("Enter principal amount: "))
+t = float(input("Enter time in years: "))
+r = float(input("Enter rate of interest: "))
+
+si = (p*t*r)/100
+
+print("The simple interest calculated is ", si)
