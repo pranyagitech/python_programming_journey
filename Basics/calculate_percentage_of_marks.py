ea48b0cc11f04fb05e86ffbd4a284a0a5d45e6f6
Pranya Agrawal <pranya.ag@gmail.com>
2026-07-12 21:54:00 +0530

    Add program to calculate percentage of marks

---
+++ Basics/calculate_percentage_of_marks.py
@@ -0,0 +1,9 @@
+m1 = int(input("Enter marks of subject 1 out of 100: "))
+m2 = int(input("Enter marks of subject 2 out of 100: "))
+m3 = int(input("Enter marks of subject 3 out of 100: "))
+m4 = int(input("Enter marks of subject 4 out of 100: "))
+m5 = int(input("Enter marks of subject 5 out of 100: "))
+
+per = (m1 + m2 +m3 +m4 +m5)/500 *100
+
+print("Your calculated percentage is ", per)
