ff125897f5ac3104ad92736cbc7884b028efd1bc
Pranya Agrawal <pranya.ag@gmail.com>
2026-08-04 00:28:56 +0530

    Add prime number checker

---
+++ LOOPS/prime_numbers_till_n.py
@@ -0,0 +1,14 @@
+n = int(input("Enter the limit: "))
+
+print("Prime Numbers:")
+
+for num in range(2, n + 1):
+    prime = True
+
+    for i in range(2, num):
+        if num % i == 0:
+            prime = False
+            break
+
+    if prime:
+        print(num)
